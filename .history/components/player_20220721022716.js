45bfe2b08b000757a8ca8bd1de7749805d14bb7e
Sehaj bindra <sehajbindra1234@gmail.com>
2022-07-21 02:28:03 +0530

    player1

--- .history/components/player_20220721022716.js
+++ .history/components/player_20220721022716.js
@@ -68,7 +68,7 @@ function player() {
       fetchCurrentSong();
       setVolume(50);
     }
-  }, [currentTrackIdState, useSpotify, useSession]);
+  }, [useRecoilState(currentTrackIdState), useSpotify, useSession]);
   return (
     <div className="h-24 bg-gradient-to-b from-black to bg-gray-900 text-white grid grid-cols-3  text-xs md:text-base px-2 md:px-8">
       {/* left */}
